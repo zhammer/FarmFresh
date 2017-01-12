2dfa616aa072a8e795d7dac627e474aa96dad26e
zhammer <zachary_hammer@brown.edu>
2017-01-11 22:31:15 -0500

    Built some components. Converting to ES6

--- app/index.js
+++ app/index.js
@@ -1,6 +1,6 @@
-var React = require('react');
+var React = require('react')
 var ReactDOM = require('react-dom');
-var routes = require('./config/routes')
+var routes = require('./config/routes.jsx')
 
 ReactDOM.render(
     routes,
